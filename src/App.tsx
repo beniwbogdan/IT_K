77d24e25d63256dc67810efd66795021d0d57171
beniv_bohdan <bohdanbeniv@gmail.com>
2022-05-31 23:00:45 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,58 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            Hi React
+            <Rating/>
+            <Accordion/>
+        </div>
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Tree/>
+            <Tree/>
+            <Tree/>
+            <Tree/>
+            <div>list of parameters 1</div>
+            <div>list of parameters 2</div>
+            <div>list of parameters 3</div>
+            <div>list of parameters 4</div>
+            <div>list of parameters 5</div>
+            <div>list of parameters 6</div>
+        </div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+
+        </div>
+    );
+}
+
+function Tree() {
+    return (
+
+        <div className="Tree">
+            <div>tree</div>
+            <div>tree</div>
+            <div>tree</div>
+        </div>
+    )
+
 }
 
 export default App;
+
